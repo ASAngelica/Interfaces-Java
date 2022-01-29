126eac9f8a103051b94d6a31eb55151d54849393
ASAngelica <asa196549@gmail.com>
2022-01-29 13:39:34 -0300

    Created projet

---
+++ src/model/services/RentalService.java
@@ -0,0 +1,45 @@
+package model.services;
+
+import java.util.concurrent.TimeUnit;
+
+import model.entities.CarRental;
+import model.entities.Invoice;
+
+public class RentalService {
+	
+	private Double pricePerDay;
+	private Double pricePerHour;
+	
+	private BrazilTaxService taxService;
+	
+	//alguns frameworks exigem que tenha o construtor padrão nas classes de entidades
+	public RentalService() {
+	}
+
+	public RentalService(Double pricePerDay, Double pricePerHour, BrazilTaxService taxService) {
+		this.pricePerDay = pricePerDay;
+		this.pricePerHour = pricePerHour;
+		this.taxService = taxService;
+	}
+	
+	public void processInvoice(CarRental carRental) {
+		long t1 = carRental.getStart().getTime();
+		long t2 = carRental.getFinish().getTime();
+		double hours = (double) (t2 - t1) / 1000 / 60 / 60;
+		
+		double basicPayment;
+		if(hours <= 12.0) {
+			basicPayment = Math.ceil(hours) * pricePerHour;
+		}
+		else {
+			basicPayment = Math.ceil(hours / 24) * pricePerDay;
+		}
+		
+		double tax = taxService.tax(basicPayment);
+		
+		carRental.setInvoice(new Invoice(basicPayment, tax));
+	}
+	
+	
+	
+}
